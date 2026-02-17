4da592e6a33457a2cf1c114d0cce69fa82dbb5bb
Kashishmittal29 <tiyakashish5@gmail.com>
2026-02-17 22:52:37 +0530

    data Types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 14553
+let email = "tiyakashish5@gmail.com"
+var password = "12345"
+accountCity =  "haryana"
+console.log(accountId);
+console.table([accountId,email,password,accountCity])
+/* prefer not to use var because of  issue in block scope and functional scope */
