c83b17f1960c886a841e19599d8900d1dc2b347a
Kashishmittal29 <tiyakashish5@gmail.com>
2026-02-18 23:12:48 +0530

    operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -51,8 +51,8 @@ let str3 = str1 + str2
 // console.log(+true);
 // console.log(+"");
 
-let num1,num2,num3
-num1 = num2 = num3 = 2+2
-let gameCounter = 100
-gameCounter++
-console.log(gameCounter);
+// let num1,num2,num3
+// num1 = num2 = num3 = 2+2
+// let gameCounter = 100
+// gameCounter++ or ++gameCounter
+// console.log(gameCounter);
