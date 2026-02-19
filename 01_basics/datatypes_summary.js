0ee0ccafa8d31594377d36b5312ab457749831fd
Kashishmittal29 <tiyakashish5@gmail.com>
2026-02-19 12:24:52 +0530

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,17 @@
+// Primitive
+// 7 types-> Number,string, boolean, null, undefined, Symbol, BigInt
+
+//Reference(Non primitive)
+//  objects, Arrays,Functions
+
+const heroes  =["shaktiman", "dogo","powerrangers"]
+let myObj={
+    name:"kashish",
+    age: 20,
+
+}
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof myFunction);
